d655445b9c6d60a48ca5f27f17ecb91631ac4c30
Thomas Broyer <t.broyer@ltgt.net>
2018-05-20 18:35:35 +0200

    Fix ktlint tasks' glob pattern

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,7 +123,7 @@ val verifyKtlint by tasks.creating(JavaExec::class) {
     description = "Check Kotlin code style."
     classpath = ktlint
     main = "com.github.shyiko.ktlint.Main"
-    args("**./*.gradle.kts", "**/*.kt")
+    args("**/*.gradle.kts", "**/*.kt")
 }
 tasks["check"].dependsOn(verifyKtlint)
 
